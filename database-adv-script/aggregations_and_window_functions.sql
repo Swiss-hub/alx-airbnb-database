55efb9fff4bd2ced433d2577bacfe641ec2ab323
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-05-21 16:35:40 +0100

    Add DB Queries Here

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -144,5 +144,11 @@ SELECT
 FROM bookings
 GROUP BY property_id;
 
-
+-- 3. Row number of properties based on total bookings using ROW_NUMBER()
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
+FROM bookings
+GROUP BY property_id;
 
